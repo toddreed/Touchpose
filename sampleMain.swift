26905b2edad397c83f3d6673975b5073c0791fac
Maximus <mxmccann@gmail.com>
2015-05-07 14:24:18 +0300

    Add the sampleMain.swift file

---
+++ sampleMain.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  APP_NAME
+//
+//  Created by USER on DATE.
+//  Copyright (c) YEAR COMPANY. All rights reserved.
+//
+// rename this file to main.swift in your project
+
+UIApplicationMain(Process.argc, Process.unsafeArgv, NSStringFromClass(QTouchposeApplication.self), NSStringFromClass(AppDelegate.self))
